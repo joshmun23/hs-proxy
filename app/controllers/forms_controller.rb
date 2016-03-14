49857a75596e53e95e2ed9c998fddf819472958c
Joshua Mun <joshmun23@gmail.com>
2016-03-14 10:32:06 -0400

    fix subscriber status

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -21,7 +21,7 @@ class FormsController < ApplicationController
 
     if params['email'] && !params['email'].empty?
       msg[:hsStatus] = 200
-      totalParams += "email=#{params['email']}&newsletter_subscriber=True&lifecyclestage=lead"
+      totalParams += "email=#{params['email']}&newsletter_subscriber=True&lifecyclestage=subscriber"
       http = Curl.post("#{url}/#{baseParams + totalParams}", hs_context)
     else
       msg[:hsStatus] = 400
